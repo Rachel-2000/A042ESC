684c0f49982308628db6da4ad18e4cdd1ff01c2e
Rachel-2000 <ruy015@ucsd.edu>
2024-03-16 22:18:24 -0700

    readme

---
+++ src/gshare_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+traces=("fp_1.bz2" "fp_2.bz2" "int_1.bz2" "int_2.bz2" "mm_1.bz2" "mm_2.bz2")
+
+# ghis_values=(11 12 13)
+ghis_values=(7 10 13 16)
+
+for trace in "${traces[@]}"
+do
+    echo "***** Running trace: $trace *****"
+    echo
+    
+    for ghis in "${ghis_values[@]}"
+    do
+        echo "Running gshare with ghis=$ghis"
+        bunzip2 -kc "../traces/$trace" | ./predictor --gshare:$ghis
+        echo
+    done
+    
+done
